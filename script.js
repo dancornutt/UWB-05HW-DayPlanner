0a24d24d13e0fe6b9ea9925a1ebbcc2db5d9295d
Dan Cornutt <dacornutt@gmail.com>
2020-09-30 16:17:51 -0700

    Inital Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+//Applies correct time class style to all blocks
+function loadTimeClass() {
+    continue;
+}
+
+//Update UI based on data in local storage
+function refreshPlanner() {
+
+}
+
+//Saves apppointment information of time block in localStorage
+function saveApt() {
+
+}
+
+//click listener for apt block editing
+
+
+refreshPlanner();
+loadTimeClass();
