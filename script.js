16a1a161e7f6d6f8a6e706f9a3b8dfaddd40ef2f
Dan Cornutt <dacornutt@gmail.com>
2020-10-04 11:29:58 -0700

    html and css are correct. Next steps styling for time and pushing data to local storage

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-//Applies correct time class style to all blocks
+let dayPlanner;
+let currTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+//Applies correct hour class style to all blocks
 function loadTimeClass() {
     continue;
 }
 
 //Update UI based on data in local storage
 function refreshPlanner() {
-
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    dayPlanner = JSON.parse(localStorage.getItem("planner"));
+    if (!dayPlanner) {
+        dayPlanner = {
+            9: "",
+            10: "",
+            11: "",
+            12: "",
+            1: "",
+            2: "",
+            3: "",
+            4: "",
+            5: "",
+        }
+    }
 }
 
-//Saves apppointment information of time block in localStorage
+//Saves apppointment information of hour block in localStorage
 function saveApt() {
-
+    
 }
 
 //click listener for apt block editing
